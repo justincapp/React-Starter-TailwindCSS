627c6601c1d608e919f15fe326cc62b5fc17bbba
azubayle <59139157+azubayle@users.noreply.github.com>
2022-01-27 17:41:31 +0100

    some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,8 @@
-import {
-  ExclamationIcon,
-  HomeIcon,
-  MenuAlt2Icon,
-  MenuIcon,
-  PlusIcon,
-  UserIcon,
-  XCircleIcon,
-  XIcon,
-} from "@heroicons/react/solid";
-import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import AppBar from "../components/AppBar";
-import Feed from "../components/Feed";
-import Menu from "../components/Menu";
-import SideNav from "../components/SideNav";
-import { AuthContext } from "../helpers/AuthContext";
-
 export default function Home() {
-  const { authState } = useContext(AuthContext);
-  const [close, setClose] = useState(false);
 
   return (
-    <div className="bg-[#0d0b13] ">
-   
+    <div className="flex items-center h-screen justify-center">
+        <h1 className="text-white text-4xl text-black">Tailwind est installé.</h1>
     </div>
   );
 }
